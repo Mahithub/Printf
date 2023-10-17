d9a1cf040ab19d055a4279d61cb4f1ed65db2044
Zelalem23 <ethiozelalem23@gmail.com>
2023-10-17 07:04:30 -0700

    printf

---
+++ int_to_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * reverse_string - reverses the given string
+ * @s: the string value that get reversed
+ * int_to_string - convert to reversed string from the value of int
+ * @num: reverse the num value
+ * Return: s
+ */
+
+void reverse_string(char *s)
+{
+	int begin, terminate;
+	char temp;
+
+	terminate = strlen(s) - 1;
+	for (begin = 0; begin < terminate; begin++, terminate--)
+	{
+		temp = s[begin];
+		s[begin] = s[terminate];
+		s[terminate] = temp;
+	}
+}
+char *int_to_string(int num)
+{
+	char *s;
+	long n = num;
+	int i = 0;
+
+	s = malloc(12);
+	if (number < 0)
+		n = -n;
+
+	while (n != 0)
+	{
+		s[i++] = '0' + n % 10;
+		n /= 10;
+	}
+	if (!s)
+		return (NULL);
+	if (number == 0)
+	{
+		*s = '0';
+		*(s + 1) = '\0';
+		return (s);
+	}
+		if (number < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse_string(s);
+	return (s);
+}
---
+++ printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * handle_specifiers - handles character
+ * @format: is a character string composed of zero or more directives
+ * @args: variadic argument function
+ * Return: int
+ */
+
+int handle_specifiers(const char **format, va_list *args)
+{
+	int c;
+
+	switch (*(++(*format)))
+	{
+		case '%':
+			c = print(1, "%", 1);
+			break;
+case 'c':
+			c = print_char(args);
+			break;
+		case 's':
+			c = print_string(args);
+			break;
+		case ' ':
+			c = -1;
+			break;
+		case 'i':
+		case '\0':
+			c = -1;
+			break;
+		case 'd':
+			c = print_int(args);
+			break;
+		default:
+			--(*format);
+			c = write(1, *format, 1);
+			break;
+	}
+	return (c);
+}
+
+/**
+ * _printf - printf standard mimics
+ * @format: string for specifying format
+ *
+ * Return:print character number or on failure -1
+ */
+
+int _printf(const char *format, ...)
+{
+	int b; 
+Int add = 0;
+	va_list args;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			b = handle_specifiers(&format, &args);
+			if (b == -1)
+				return (b);
+			add += b;
+			format++;
+		}
+		else
+		{
+			b = write(1, format, 1);
+			if (b == -1)
+				return (b);
+			add++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (add);
+}
